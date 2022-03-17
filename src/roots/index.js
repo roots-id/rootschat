ea82d9a300d588adcf3bdce730b120ad73e20547
2byrds <lance.byrd@rootsid.com>
2022-03-17 16:45:44 -0400

    faked wal cli example, setup

--- src/roots/index.js
+++ src/roots/index.js
@@ -1,5 +1,7 @@
 import rwLogo from '../assets/LogoOnly1024.png'
-import personLogo from '../assets/smallBWPerson.png'
+import perLogo from '../assets/smallBWPerson.png'
+
+const currentTime = new Date().getTime();
 
 const members1 = [
     {
@@ -43,12 +45,13 @@ const channels = [
 //https://lh5.googleusercontent.com/bOG9vTJDA73jNwAtwm1ioc__Nr1Ch199Xo-4R9xFgJW_hsMsNwef2WQCwm-8_c9d3B8zF7vSEF5E-nLIMOOaZJlPz_dKAo-j_s102ddaNla0iiywfT2fAljxrsdrkxDllg=w1280
 //https://lh5.googleusercontent.com/iob7iL2ixIzrP24PvQVJjpnmt3M2HvJIS7E3mIg2qWRMIJIlnIo27qjAS4XL9tC3ZwhZ78sbpwygbK2hDjx-8z2u_WaunTLxpEFgHJngBljvF8VvJ3QoAiyVfjEmthEEWQ=w1280
 export const rootsLogo = rwLogo;
+export const personLogo = perLogo;
 
 const users = [
     {
         id: "RootsWalletBot1",
         displayName: "RootsWallet",
-        displayPictureUrl: rwLogo,
+        displayPictureUrl: rootsLogo,
     },
     {
         id: "TestUser1",
@@ -57,47 +60,201 @@ const users = [
     },
 ];
 
+const walCliCommands=[
+" ./wal.sh new-wallet holder_wallet -m poet,account,require,learn,misery,monitor,medal,great,blossom,steak,rain,crisp",
+"./wal.sh new-did holder_wallet holder_did",
+"./wal.sh new-did issuer_wallet issuer_did -i",
+"./wal.sh publish-did issuer_wallet issuer_did",
+"./wal.sh show-did holder_wallet holder_did",
+"./wal.sh issue-cred issuer_wallet issuer_did did:prism:654a4a9113e7625087fd0d3143fcac05ba34013c55e1be12daadd2d5210adc4d:Cj8KPRI7CgdtYXN0ZXIwEAFKLgoJc2VjcDI1NmsxEiEDA7B2nZ_CvcIdkU2ovzBEovGzjwZECMUeHUeNo5_0Jug credential_a",
+"./wal.sh verify-cred issuer_wallet issued credential_a",
+"./wal.sh export-cred issuer_wallet credential_a credential_a.json",
+"cat credential_a.json",
+"./wal.sh import-cred holder_wallet credential_a credential_a.json",
+"./wal.sh verify-cred holder_wallet imported credential_a",
+"./wal.sh revoke-cred issuer_wallet credential_a",
+"./wal.sh verify-cred issuer_wallet issued credential_a",
+]
+
+const walCliMsgs = [
+    {
+      body: "wallet created",
+      type: "status",
+          createdTime: currentTime+986,
+          user: users[0],
+    },
+    {
+      body: "DID created",
+      type: "status",
+          createdTime: currentTime+987,
+          user: users[0],
+    },
+    {
+      body: "DID created",
+      type: "status",
+          createdTime: currentTime+988,
+          user: users[0],
+    },
+    {
+      body: "https://explorer.cardano-testnet.iohkdev.io/en/transaction?id=1f4f19f2016c4468777da24a5656b9b009550a601192960e22f1233af4e8b3ef",
+      type: "blockchainUri",
+          createdTime: currentTime+989,
+          user: users[0],
+    },
+    {
+        body: JSON.stringify({alias: "holder_did",
+            didIdx: 0,
+            uriCanonical: "did:prism:654a4a9113e7625087fd0d3143fcac05ba34013c55e1be12daadd2d5210adc4d",
+            uriLongForm: "did:prism:654a4a9113e7625087fd0d3143fcac05ba34013c55e1be12daadd2d5210adc4d:Cj8KPRI7CgdtYXN0ZXIwEAFKLgoJc2VjcDI1NmsxEiEDA7B2nZ_CvcIdkU2ovzBEovGzjwZECMUeHUeNo5_0Jug",
+            operationHash: "",
+            keyPairs: [
+                {
+                    keyId: "master0",
+                    didIdx: 0,
+                    keyType: 0,
+                    keyIdx: 0,
+                    privateKey: "9c2a64d860cb86ce0af23787fccd2ad12a73d5e758c706d8567de49dec2ec029",
+                    publicKey: "0403b0769d9fc2bdc21d914da8bf3044a2f1b38f064408c51e1d478da39ff426e884c34858bcfa2afbd3cc4e4b1a8d3fc848b74f92360e91729aaf8d77d8207963",
+                    revoked: false
+                }
+            ]
+        }),
+        type: "jsonDid",
+            createdTime: currentTime+990,
+            user: users[0],
+    },
+    {
+      body: "https://explorer.cardano-testnet.iohkdev.io/en/transaction?id=0ce00bc602ef54dfc52b4106bebcafb72c2447bdf666cd609d50fd3a7e9d2474",
+      type: "blockchainUri",
+          createdTime: currentTime+991,
+          user: users[0],
+    },
+    {
+      body: "Credential issued",
+      type: "status",
+          createdTime: currentTime+992,
+          user: users[0],
+    },
+    {
+      body: "Valid credential.",
+      type: "status",
+          createdTime: currentTime+993,
+          user: users[0],
+    },
+    {
+      body: "Credential exported",
+      type: "status",
+          createdTime: currentTime+994,
+          user: users[0],
+    },
+    {
+      body: JSON.stringify({
+                 encodedSignedCredential: "eyJpZCI6ImRpZDpwcmlzbTozYWU4YmFkYzAzMWEzZjU2YjlmYzZiNWEwMmVhNDczNWJmY2RmM2I5ODFkODc4NjBkNWMxNjkzYzBkODA2OGJiIiwia2V5SWQiOiJpc3N1aW5nMCIsImNyZWRlbnRpYWxTdWJqZWN0Ijp7Im5hbWUiOiJBbGljZSIsImRlZ3JlZSI6IkNvbXB1dGVyIFNjaWVuY2UiLCJkYXRlIjoiMjAyMi0wMy0xMCAxNzoxNjo1OCIsImlkIjoiZGlkOnByaXNtOjY1NGE0YTkxMTNlNzYyNTA4N2ZkMGQzMTQzZmNhYzA1YmEzNDAxM2M1NWUxYmUxMmRhYWRkMmQ1MjEwYWRjNGQ6Q2o4S1BSSTdDZ2R0WVhOMFpYSXdFQUZLTGdvSmMyVmpjREkxTm1zeEVpRURBN0IyblpfQ3ZjSWRrVTJvdnpCRW92R3pqd1pFQ01VZUhVZU5vNV8wSnVnIn19.MEUCIQC8WiVfl6nH-DIBdK9SN0SzqYNN49WnuECk77V8-vUIQwIgHLZUkCRivnv7NiIurd2YR5MWjUUIbbKHDJovnbexeNI",
+                 proof: {
+                     "hash": "bbb3fee220db35c2fc717ca61e0e55e2f670cfb238ff0484ea768dd1aaf23522",
+                     "index": 0,
+                     "siblings": [
+                     ]
+                 }
+             }),
+      type: "jsonCredential",
+          createdTime: currentTime+995,
+          user: users[0],
+    },
+    {
+      body: "Credential imported",
+      type: "status",
+          createdTime: currentTime+996,
+          user: users[0],
+    },
+    {
+      body: "Valid credential.",
+      type: "status",
+          createdTime: currentTime+997,
+          user: users[0],
+    },
+    {
+      body: "https://explorer.cardano-testnet.iohkdev.io/en/transaction?id=0ce00bc602ef54dfc52b4106bebcafb72c2447bdf666cd609d50fd3a7e9d2474",
+      type: "blockchainUri",
+          createdTime: currentTime+998,
+          user: users[0],
+    },
+    {
+      body: "Credential revoked",
+      type: "status",
+          createdTime: currentTime+999,
+          user: users[0],
+    },
+    {
+      body: "Invalid credential.",
+      type: "status",
+    createdTime: currentTime+1000,
+    user: users[0],
+    }
+]
+
+var walCommandsMsgs =  walCliMsgs.reduce(function(result, message, index) {
+  result.push(message);
+  result[index]["command"]=walCliCommands[index];
+  result[index]["id"]="walCli"+index
+  return result;
+}, []);
+
 export function getFakeUser() {
     return users[1];
 }
 
-export function getRootsWalletUser() {
+export function getRootsWalletBot() {
     return users[0];
 }
 
+const achievementIntroMsg = "You have a new achievement: ";
 const achievementMsgs = [
     {
       id: "message1",
-      body: "Achievement: Opened RootsWallet!",
+      body: achievementIntroMsg+"Opened RootsWallet!",
       type: "text",
-      createdTime: "1",
+      createdTime: currentTime,
       user: users[0],
     },
+
     {
       id: "message2",
-      body: "{subject: you,issuer: RootsWallet,credential: Clicked Example}",
+      body: "{subject: you,issuer: RootsWallet,credential: Opened RootsWallet}",
       type: "jsonCredential",
-      createdTime: "2",
+      createdTime: currentTime+1000,
       user: users[0],
     },
     {
       id: "message3",
-      body: "Achievement: Clicked Example",
+      body: achievementIntroMsg+"Clicked Example!",
       type: "text",
-      createdTime: "3",
+      createdTime: currentTime+2000,
       user: users[0],
     },
     {
       id: "message4",
       body: "{subject: you,issuer: RootsWallet,credential: Clicked Example}",
       type: "jsonCredential",
-      createdTime: "4",
+      createdTime: currentTime+3000,
+      user: users[0],
+    },
+];
+const comingSoonMsgs = [
+    {
+      id: "message1",
+      body: "Coming Soon....",
+      type: "text",
+      createdTime: currentTime,
       user: users[0],
     },
 ];
 
 const channelsMessages = []
 channelsMessages[channels[0].id] = achievementMsgs;
+channelsMessages[channels[1].id] = comingSoonMsgs;
+channelsMessages[channels[2].id] = comingSoonMsgs;
+channelsMessages[channels[3].id] = walCommandsMsgs;
 
 //TODO log public DIDs and/or create Pairwise DIDs
 export function createChannel (channel) {
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -2,9 +2,12 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Bubble, ChatInput, GiftedChat, InputToolbar, SendButton } from 'react-native-gifted-chat';
 import { View } from 'react-native';
 
-import { getFakeUser, getMessages, sendMessage, startChatSession } from '../roots';
+import { getFakeUser, getMessages, rootsLogo, sendMessage, startChatSession } from '../roots';
 import Loading from '../components/Loading';
 import { AuthContext } from '../navigation/AuthProvider';
+
+//import emojiUtils from 'emoji-utils';
+//import SlackMessage from '../components/SlackMessage';
 
 export default function ChatScreen({ route }) {
   //useContext(AuthContext)
@@ -53,7 +56,8 @@ export default function ChatScreen({ route }) {
             {...props}
             wrapperStyle={{
               left: {
-                backgroundColor: '#d3d3d3',
+                backgroundColor: '#fad58b',
+                color: '#222222',
               },
             }}
         />
@@ -65,14 +69,44 @@ export default function ChatScreen({ route }) {
           <InputToolbar
               {...props}
                   containerStyle={{
-                    backgroundColor: "222222",
+                    backgroundColor: "#222222",
                     borderTopColor: "#dddddd",
                     borderTopWidth: 1,
-                    padding: 1
+                    padding: 1,
                   }}
           />
       );
     }
+
+//  function renderMessage(props) {
+//    const {
+//      currentMessage: { text: currText },
+//    } = props
+//
+//    let messageTextStyle
+//
+//    // Make "pure emoji" messages much bigger than plain text.
+//    if (currText && emojiUtils.isPureEmojiString(currText)) {
+//      messageTextStyle = {
+//        fontSize: 28,
+//        // Emoji get clipped if lineHeight isn't increased; make it consistent across platforms.
+//        lineHeight: Platform.OS === 'android' ? 34 : 30,
+//      }
+//    }
+//
+//    return <SlackMessage {...props} messageTextStyle={messageTextStyle} />
+//  }
+
+//  function renderMessageImage(props) {
+//    console.log("Rendering message image",props);
+//    return (
+//      <MessageImage
+//        {...props}
+//        style={styles.image}
+//        source={require('../assets/LogoOnly1024.png')}
+//      />
+//    )
+//  }
 
   if (loading) {
     return <Loading />;
@@ -84,9 +118,11 @@ export default function ChatScreen({ route }) {
       <GiftedChat
           messages={messages.sort((a, b) => b.createdAt - a.createdAt)}
           onSend={handleSend}
-          user={mapUser(user)}
           renderBubble={renderBubble}
-          showUserAvatar={true}
+          renderInputToolbar={renderInputToolbar}
+          renderAllAvatars={true}
+          renderAvatarOnTop={true}
+          user={mapUser(user)}
       />
     </View>
   );
